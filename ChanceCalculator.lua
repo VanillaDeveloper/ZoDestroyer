b077827ce3ad8e5c814181f7e0c48198f7fe1664
aeth <86308270+VanillaDeveloper@users.noreply.github.com>
2023-04-20 13:23:48 +0200

    Create ChanceCalculator.lua

---
+++ ChanceCalculator.lua
@@ -0,0 +1,24 @@
+local ProbabilityCalculator = {}
+ProbabilityCalculator.__index = ProbabilityCalculator
+
+function ProbabilityCalculator.calculate(chances)
+	task.spawn(function()
+		local total = 0
+		for _, chance in pairs(chances) do
+			total += chance
+		end
+
+		local randomValue = math.random(1, total)
+		local currentTotal = 0
+		for name, chance in pairs(chances) do
+			currentTotal += chance
+			if randomValue <= currentTotal then
+				return name
+			end
+		end
+
+		return nil
+	end)
+end
+
+return ProbabilityCalculator
